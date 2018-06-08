fea0de5facd6936568b3e31a3bb033a236631ddf
高新凯 <467469274@qq.com>
2018-06-08 13:31:57 +0800

    2018年06月08日13:31:57

--- js/index.js
+++ js/index.js
@@ -1,23 +1,28 @@
-var dappAddress = "n1w1VwdkHvuUorpmTifsHmH8VJBnXaNmtra";
+var dappAddress = "5cea41ec261609a920f7f49be5304985a7dcc40a950dc9157dd86e364878fd2f";
 var NebPay = require("nebpay");
 var nebPay = new NebPay();
 
 
 
 
-$('.get').on('click',function (data) {
-    nebPay.simulateCall(dappAddress, "0", "get", JSON.stringify([]), {
-        listener: function(res) {
-            console.log(res)
-        }
-    });
-})
-$('.set').on('click',function (data) {
-    "use strict";
-    nebPay.call(dappAddress, "0", "set", JSON.stringify([]), {
-        listener: function(res){
-            if (res.txhash) {
+$(function () {
+    $('.get').on('click',function (data) {
+        console.log(1)
+        nebPay.simulateCall(dappAddress, "0", "get", JSON.stringify(['qweqe']), {
+            listener: function(res) {
+                console.log(res)
             }
-        }
-    });
+        });
+    })
+    $('.set').on('click',function (data) {
+        "use strict";
+        console.log(2)
+        nebPay.call(dappAddress, "0.001", "set", JSON.stringify(['eqee']), {
+            listener: function(res){
+                if (res.txhash) {
+                    console.log('qweqe',res)
+                }
+            }
+        });
+    })
 })
